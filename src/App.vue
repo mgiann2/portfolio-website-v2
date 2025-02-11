ac20b757c6fa25f0ca341933ad3cbb745b0bb89d
Matteo Giannone <matteogiannone24@gmail.com>
2025-02-11 10:10:32 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Hero from "./components/Hero.vue"
+</script>
+
+<template>
+    <Hero/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Hero.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="container">
+        <h1>Hello</h1>
+        <button>Press Me!</button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
